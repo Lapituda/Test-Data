f7497ec5d043873b00d483ada76db49b05994d51
Lapituda <102392241+Lapituda@users.noreply.github.com>
2024-02-22 02:00:07 -0600

    Update visualizacion.sql

--- visualizacion.sql
+++ visualizacion.sql
@@ -1,24 +1,20 @@
--- Consulta 1
-
-select * from muestras
-
 SELECT 
+    MU.anio,
     MU.clave_sitio,
     CASE
         WHEN DS.nombre_sitio IS NULL THEN 'Nombre no disponible'
         ELSE DS.nombre_sitio
-    END as nombre_sitio,
-    MU.clave_monitoreo,
-    MU.fecha_realizacion,
-    MU.anio,
-    MU.co3,
-    MU.hco3,
-    MU.oh,
-    MU.cot,
-    MU.cot_sol,
-    MU.n_nh3,
-    MU.n_no2  
+    END AS nombre_sitio,
+    AVG(MU.co3) AS avg_co3,
+    AVG(MU.hco3) AS avg_hco3,
+    AVG(MU.oh) AS avg_oh,
+    AVG(MU.cot) AS avg_cot,
+    AVG(MU.cot_sol) AS avg_cot_sol,
+    AVG(MU.n_nh3) AS avg_n_nh3,
+    AVG(MU.n_no2) AS avg_n_no2
 FROM dbo.muestras AS MU
 LEFT JOIN dbo.sitios AS DS
     ON MU.clave_sitio = DS.clave_sitio
-WHERE DS.nombre_sitio IS NULL;
+WHERE MU.anio >= 2019 
+GROUP BY MU.anio, MU.clave_sitio, DS.nombre_sitio
+HAVING AVG(MU.n_no2) > 1;
